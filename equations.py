943bcd4e1397f5eb0d8a7589a4ac9814680463ad
ellenfurman <ellenf205@gmail.com>
2021-03-17 17:19:53 +0200

    seconed

--- equations.py
+++ equations.py
@@ -1,8 +1,8 @@
 
 def XtimesY(x,y):
     if x<=0:
-        return 0
-    number=exponent(y*ln(x))
+        return 0.0
+    number=exponent(y*Ln(x))
     return(number)
 
 def exponent(x):
@@ -19,7 +19,9 @@ def exponent(x):
         e_x=e_x+x_n/assembly  
     return(e_x)
 
-def ln(x):
+def Ln(x):
+    if x<=0:
+        return 0.0
     y=0
     y_n1=0
     while y<1000:
@@ -28,13 +30,15 @@ def ln(x):
     return(y_n1)
 
 def sqrt(x,y):
+    if y<=0 or x==0:
+        return 0.0
     outcome=XtimesY(y, 1/x)
     return(outcome)
 
 def calculate(x):
+    if x<=0:
+        return 0.0
     calc=exponent(x)*XtimesY(7,x)*XtimesY(x, -1)*sqrt(x,x)
     return(calc)
     
-x=float(input())
-print(calculate(x))
     
